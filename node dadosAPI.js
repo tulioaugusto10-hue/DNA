8337cd8bda861cd70dcd1b81f342b55adf24fd9d
tulioaugusto10-hue <tulioaugusto10@gmail.com>
2026-01-29 00:59:36 -0300

    node dadosAPI.js

--- node dadosAPI.js
+++ node dadosAPI.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 const fs = require('fs');
-const path = require('path');
+const path = require('path'); 
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL || 
